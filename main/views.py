451f5dd84eacc3a43e9dc4d2f39705cda31509ce
TWpower <elld8850@naver.com>
2017-12-27 16:57:06 +0900

    Make temperatures api

--- main/views.py
+++ main/views.py
@@ -1,3 +1,23 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+import urllib.request
+import urllib.parse
+from bs4 import BeautifulSoup
+import json
+
 
 # Create your views here.
+def get_temperatures_by_json(request):
+    with urllib.request.urlopen('http://www.koreawqi.go.kr/wQSCHomeLayout_D.wq?action_type=T') as response:
+        html = response.read()
+        soup = BeautifulSoup(html, 'html.parser')
+        div_cont1 = soup.find("div", {"class": "cont1"})
+        table = div_cont1.find_all('table', {"class": "table_01"})
+        rows = table[1].find_all('tr')
+        rows.pop(0)
+
+        temperatures = []
+        for row in rows:
+            cols = row.find_all('td')
+            temperatures.append({cols[0].text: cols[1].text})
+        return HttpResponse(json.dumps(temperatures, ensure_ascii=False), content_type='application/json')
