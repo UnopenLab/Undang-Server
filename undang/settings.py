8ee8777f09937e337ed7ef0478631055d056c5ad
TWpower <elld8850@naver.com>
2018-01-06 18:15:54 +0900

    Add CORS features to server codes

--- undang/settings.py
+++ undang/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -47,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'undang.urls'
@@ -69,6 +71,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'undang.wsgi.application'
 
+# CORS Setting
+CORS_ORIGIN_ALLOW_ALL = True
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
