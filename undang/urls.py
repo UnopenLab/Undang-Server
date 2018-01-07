78c187b9bfd2cc6d1b97b20ff6e56a5bc030b59d
TWpower <elld8850@naver.com>
2018-01-07 16:02:16 +0900

    Add index.html to Server

--- undang/urls.py
+++ undang/urls.py
@@ -13,11 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls import url, include
 from main import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'', include('main.urls')),
     url(r'^temperatures', views.get_temperatures_by_json, name='get_temperatures_by_json')
 ]
